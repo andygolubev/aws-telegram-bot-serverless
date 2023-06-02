ef7794d77c03e6cc3afa6e4d86a027848754aae0
Andy Golubev <verkomf@gmail.com>
2023-06-02 16:31:55 +0300

    debug lambda layers

--- terraform/02-lambda.tf
+++ terraform/02-lambda.tf
@@ -1,17 +1,39 @@
+
+## CREATE a Layer for the bot-lambda ##
+
+resource "null_resource" "pip-install" {
+    provisioner "local-exec" {
+      command = <<-EOF
+      mkdir -p /tmp/packages/python/
+      python3 -m venv /tmp/temp-venv
+      source /tmp/temp-venv/bin/activate
+      pip3 install -r ../lambda-bot/bot-function/requirements.txt -t /tmp/packages/python/
+      EOF
+    }
+}
+
 data "archive_file" "layer-zip-file" {
     type = "zip"
 
-    source_dir  = "../lambda-bot/packages/"
+    source_dir  = "/tmp/packages/"
     output_path = "/tmp/layer-package.zip"
-}
 
+    depends_on = [ null_resource.pip-install,]
+}
 
 resource "aws_lambda_layer_version" "lambda-layer-for-packages" {
     filename   = "/tmp/layer-package.zip"
     layer_name = "lambda-layer-for-packages"
 
     compatible_runtimes = ["python3.10"]
+
+    depends_on = [ data.archive_file.layer-zip-file, ]
 }
+
+
+
+
+## CREATE the bot-lambda ##
 
 data "archive_file" "lambda-bot-zip-file" {
     type = "zip"
@@ -21,8 +43,6 @@ data "archive_file" "lambda-bot-zip-file" {
 }
 
 resource "aws_lambda_function" "bot-lambda" {
-  # If the file is not in the current working directory you will need to include a
-  # path.module in the filename.
   filename      = "/tmp/lambda-bot.zip"
   function_name = "bot-lambda"
   role          = aws_iam_role.lambdaRole.arn
@@ -43,8 +63,13 @@ resource "aws_lambda_function" "bot-lambda" {
       AWS_CLOUD_REGION = var.aws_region
     }
   }
+
+  depends_on = [ data.archive_file.lambda-bot-zip-file, ]
 }
 
+
+
+## CREATE the webhook-lambda ##
 
 data "archive_file" "webhook-function-zip-file" {
     type = "zip"
@@ -76,5 +101,7 @@ resource "aws_lambda_function" "webhook-lambda" {
       CALLBACK_URL = aws_apigatewayv2_api.call-back-api.api_endpoint
     }
   }
+
+  depends_on = [ data.archive_file.webhook-function-zip-file, ]
 }
 
